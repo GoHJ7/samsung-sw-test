cf5abba84a5622dff35170f8abd045e0557fe12b
GoHJ7 <pdsfko@gmail.com>
2023-10-12 23:13:29 +0900

    5650:solved

--- ss_기출/5650_핀볼.cpp
+++ ss_기출/5650_핀볼.cpp
@@ -36,26 +36,23 @@
 #include <algorithm>
 #include <queue>
 #include <deque>
+#include <cstring>
 
 using namespace std;
 
-bool visited[4][100][100];
-int maxScore[4][100][100];
 int board[100][100];
+int maxScore;
 void init(){
-    for(int i =0 ; i < 4; i++){
-        for(int j =0 ; j <100; j++){
-            for(int k = 0; k <100;k++){
-                visited[i][j][k] = false;
-                maxScore[i][j][k] = 0;
-                board[j][k] = 0;
-            }
+    for(int j =0 ; j <100; j++){
+        for(int k = 0; k <100;k++){
+            board[j][k] = 0;
         }
     }
+    maxScore = 0;
 }
 
 void move(int dir, int i, int j,int n);
-int next(int& curDir, int& curI, int& curJ,deque<tuple<int,int,int>>& track ,int n);
+int next(int& curDir, int& curI, int& curJ ,int n);
 int dirX[4] = {-1,0,1,0};
 int dirY[4] = {0,1,0,-1};
 
@@ -95,26 +92,14 @@ int main(int argc, char** argv)
                 //북동남서
                 for(int dir = 0; dir <4; dir++){
                     if(board[i][j] != 0){
-                        maxScore[dir][i][j] = 0;
+                        continue;
                     }else{
                         move(dir,i,j,n);
                     }
                 }
             }
         }
-        //최댓값 확인
-        int maxS = 0;
-        for(int i = 0; i<n;i++){
-            for(int j = 0; j < n; j++){
-                //북동남서
-                for(int dir = 0; dir <4; dir++){
-                    maxS = max(maxS,maxScore[dir][i][j]);
-                    cout<<"Maxs"<<maxS<<dir<<i<<j<<endl;
-                }
-            }
-        }
-
-        cout<<"#"<<test_case<<" "<<maxS<<"\n";
+        cout<<"#"<<test_case<<" "<<maxScore<<"\n";
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
@@ -126,119 +111,65 @@ void move(int dir, int i , int j,int n){
     int curI = i;
     int curJ = j;
     
-    //dir,i,j
-    deque<tuple<int,int,int>> track;
-    
+
+
     //북동남서
     //움직임 탐색
-    cout<<"움직임 탐색시작"<<endl;
     while(true){
-        cout<<"your Cor "<<curDir<<" "<<curI<<" "<<curJ<<endl;
         if(curI < 0 || curI >= n || curJ <0 || curJ >= n){
-            maxS += next(curDir,curI,curJ,track,n);
+            maxS += next(curDir,curI,curJ,n);
+            start = false;
             continue;
         }
-        
         //움직임 끝 블랙홀 또는 시작지점
-        if((curI == i && curJ == j && !start)
-        || board[curI][curJ] == -1){
+        if((curI == i && curJ == j && !start)|| board[curI][curJ] == -1){
             //경로 저장된거 해제
-            if(!track.empty()) track.pop_back();
             break;
         }
-        //visited일시
-        if(visited[curDir][curI][curJ]){
-            maxS += maxScore[curDir][curI][curJ];
-            //마지막 경로 저장된거 해제;
-            if(!track.empty()) track.pop_back();
-            break;
-        }
-        maxS += next(curDir,curI,curJ,track,n);
+        maxS += next(curDir,curI,curJ,n);
         start = false;
     }
 
-    visited[dir][i][j] = true;
-    maxScore[dir][i][j] = maxS;
-    cout<<"현재 최대 점수"<<maxS<<endl;
-    
-    int trackMax = 0;
-    //visited정리
-    cout<<"track 정리 시작"<<endl;
-    while(!track.empty()){
-        tuple<int,int,int> temp = track.front();
-        track.pop_front();
 
-        int trackDir = get<0>(temp);
-        int trackI = get<1>(temp);
-        int trackJ = get<2>(temp);
-        
-        //벽이면 trackMax증가
-        if(trackI< 0 || trackJ < 0 
-        || trackI >= n || trackJ>= n){
-            trackMax++;
-        }
-        //빈칸이면 방문, 점수 추가,visited,maxScore처리
-        else if(board[trackI][trackJ] == 0){
-            cout<<"track 경로 빈칸"<<trackDir<<trackI<<trackJ<<endl;
-            visited[trackDir][trackI][trackJ] = true;
-            maxScore[trackDir][trackI][trackJ] = maxS - trackMax;
-        }
-        
-        //장애물이면 trackMax증가
-        else if(board[trackI][trackJ] >=1 &&board[trackI][trackJ] <=5){
-            trackMax++;
-        }
-        else{
-            cout<<"who are you"<<trackDir<<" "<<trackI<<" "<<trackJ<<endl;
-        }
-    }
+    maxScore = max(maxScore,maxS);
 }
 
 
 
-int next(int& curDir, int& curI, int& curJ,deque<tuple<int,int,int>>& track ,int n){
+int next(int& curDir, int& curI, int& curJ,int n){
     //next는 track에 경로를 넣는다. 벽포함, 장애물 포함
     //점수를 낸 만큼을 반환한다.
     //다음 움직임을 curDir,I,J에 저장한다. 장애물을 만난 경우엔 장애물 이후의 좌표를 저장한다.
-    cout<<"next coor"<<curI + dirX[curDir]<<curJ + dirY[curDir]<<board[curI + dirX[curDir]][curJ + dirY[curDir]]<<endl;
     //시작 좌표 지점
     int retScore = 0;
     //벽
     if(curI == 0 && curDir == 0){
         //북쪽 벽
-        cout<<"북벽"<<endl;
         curI += dirX[curDir];
         curJ += dirY[curDir];
         curDir = 2;
-        track.push_back(make_tuple(curDir,curI,curJ));
         retScore +=1;
         return retScore;
     }else if(curI == n-1 && curDir ==2){
         //남쪽벽
-        cout<<"남벽"<<endl;
         curI += dirX[curDir];
         curJ += dirY[curDir];
         curDir = 0;
-        track.push_back(make_tuple(curDir,curI,curJ));
-        retScore +=1;
         retScore +=1;
         return retScore;
     }else if(curJ == 0 && curDir == 3){
         //서쪽벽
-        cout<<"서벽"<<endl;
         curI += dirX[curDir];
         curJ += dirY[curDir];
         curDir = 1;
-        track.push_back(make_tuple(curDir,curI,curJ));
+        retScore +=1;
         retScore +=1;
         return retScore;
     }else if(curJ == n-1 && curDir == 1){
         //동쪽벽
-        cout<<"동벽"<<endl;
         curI += dirX[curDir];
         curJ += dirY[curDir];
         curDir = 3;
-        track.push_back(make_tuple(curDir,curI,curJ));
         retScore +=1;
         return retScore;
     }
@@ -247,10 +178,9 @@ int next(int& curDir, int& curI, int& curJ,deque<tuple<int,int,int>>& track ,int
 
     //빈칸
     if(board[curI + dirX[curDir]][curJ + dirY[curDir]] == 0){
-        cout<<"빈칸"<<endl;
+        //cout<<"빈칸"<<endl;
         curI += dirX[curDir];
         curJ += dirY[curDir];
-        track.push_back(make_tuple(curDir,curI,curJ));
         return 0;
     }
     //블록
@@ -269,7 +199,6 @@ int next(int& curDir, int& curI, int& curJ,deque<tuple<int,int,int>>& track ,int
             }else{
                 curDir = 0;
             }
-            track.push_back(make_tuple(curDir,curI,curJ));
             break;
             case 2:
             //북->동,동->서, 남->북,서->남
@@ -284,7 +213,6 @@ int next(int& curDir, int& curI, int& curJ,deque<tuple<int,int,int>>& track ,int
             }else{
                 curDir = 2;
             }
-            track.push_back(make_tuple(curDir,curI,curJ));
             break;
             case 3:
             //북->서,동->남, 남->북,서->동
@@ -299,7 +227,6 @@ int next(int& curDir, int& curI, int& curJ,deque<tuple<int,int,int>>& track ,int
             }else{
                 curDir = 1;
             }
-            track.push_back(make_tuple(curDir,curI,curJ));
             break;
             case 4:
             //북->남,동->북, 남->서,서->동
@@ -314,7 +241,6 @@ int next(int& curDir, int& curI, int& curJ,deque<tuple<int,int,int>>& track ,int
             }else{
                 curDir = 1;
             }
-            track.push_back(make_tuple(curDir,curI,curJ));
             break;
             case 5:
             //북->남,동->서, 남->북,서->동
@@ -329,7 +255,6 @@ int next(int& curDir, int& curI, int& curJ,deque<tuple<int,int,int>>& track ,int
             }else{
                 curDir = 1;
             }
-            track.push_back(make_tuple(curDir,curI,curJ));
             break;
             default:
             break;
@@ -348,7 +273,6 @@ int next(int& curDir, int& curI, int& curJ,deque<tuple<int,int,int>>& track ,int
                 if(board[curI][curJ] == board[x][y] && (curI != x || curJ != y)){
                     curI = x;
                     curJ = y;
-                    //track.push(make_tuple(curDir,curI,curJ));
                     return retScore;
                 }
             }
